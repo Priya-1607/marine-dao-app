876b7a46744c9c088e852c81981987ca9b15c2b0
Ramdas Prajapati <ramdas693@gmail.com>
2022-07-11 15:55:50 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import boat from './assets/images/boat.svg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='font-bold text-xl'>Hello World</h1>
+        <img src={boat}/>
     </div>
   );
 }
