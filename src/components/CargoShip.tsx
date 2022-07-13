61366f21264fd4cab458107b92b03dfea8f335f7
Priya-1607 <91568553+Priya-1607@users.noreply.github.com>
2022-07-13 13:43:26 +0530

    cargoShip page 2

--- src/components/CargoShip.tsx
+++ src/components/CargoShip.tsx
@@ -8,16 +8,16 @@ function CargoShip() {
                 <div className="ineer">
 
 
-                      <span className="top" >Cargo Ships</span><br/>
+                      <p className="top" >Cargo Ships</p><br/>
 
 
                     <span className="top-2">Fundrising DAO</span><br/>
-                    <p className="top-3">Mint an NFT,become a member of the community,divide profits and partake a role in the future of the company!</p>
+                    <p className="top-3">Mint an NFT,become a member of<br/> the community,divide profits and<br/> partake a role in the future<br/> of the company!</p>
 
             </div>
             </div>
 
-            <div className="right"><img src={boatGroup}/></div>
+            <div className="right flex justify-center content-center"><img src={boatGroup}/></div>
 
         </div>
 
