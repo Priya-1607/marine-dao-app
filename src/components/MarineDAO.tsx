f7e95a9146e3c9ed25b57edd8ccd56886568b76b
Ramdas Prajapati <ramdas693@gmail.com>
2022-07-13 16:31:26 +0530

    first page completed

--- src/components/MarineDAO.tsx
+++ src/components/MarineDAO.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import styles from '../App.module.css'
+import boat from '../assets/images/boat.svg'
 
 const MarineDao = () => {
     return (
-        <div className={`${styles.pageBack} grid grid-cols-4`}>
-            <div className='grid grid-cols-1 justify-center content-start pl-12 pt-12'>
+        <div className={`${styles.pageBack} flex pt-12`}>
+            <div className='justify-center content-start pl-12'>
                 <h2 className={styles.heading}>MarineDAO</h2>
                 <p className={`${styles.subHeading} py-5`}>Owning a ship has never <br/> been easier!
                 </p>
-                <p className={`${styles.description} py-5 w-50`}>
+                <p className={`${styles.description} py-5`}>
                     Mint an NFT, become a member of <br/> the community, divide profits and <br/> partake a role in the
                     future <br/>
                     of the company!
@@ -20,8 +21,9 @@ const MarineDao = () => {
 
                 </div>
             </div>
-            <div className={`${styles.back} grid col-start-2 col-span-3 content-end`}>
-                {/*<img className={styles.image} height={400} width={800} src={boat}/>*/}
+            <div className={`${styles.back}`}>
+
+                <img className={styles.image} src={boat}/>
             </div>
 
 
