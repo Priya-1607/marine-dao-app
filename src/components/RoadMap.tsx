314734ae641e7b2d9b1f9fede34e9dc96702c8fc
Priya-1607 <91568553+Priya-1607@users.noreply.github.com>
2022-07-15 19:42:06 +0530

    v4 last page

--- src/components/RoadMap.tsx
+++ src/components/RoadMap.tsx
@@ -7,16 +7,16 @@ function RoadMap() {
 
             <div className="ShipMain">
 
-            <label className="img "><img src={ships}/></label>
+            <label className="img origin-center rotate-1  "><img src={ships}/></label>
 
                 <div className="subHead">
             <p className="road">Road <p className="map">Map</p></p>
                     <div className="leftPara">
 
-            <span className="phase">Phase 1</span><p className="para">200/1500 NFTs<br/>Drop daily until September 2022</p>
-            <span className="phase">Phase 2</span><p className="para">500/1500 NFTs<br/>Drop daily until December 2022</p>
-            <span className="phase">Phase 3</span><p className="para">1000/1500 NFTs<br/>Drop daily until February 2023</p>
-            <span className="phase">Phase 4</span><p className="para">1500/1500 NFTs.Final release.<br/>Promote in April 2023</p>
+            <p className="phase">Phase 1</p><p className="para">200/1500 NFTs<br/>Drop daily until September 2022</p>
+                         <p className="phase">Phase 2</p><p className="para">500/1500 NFTs<br/>Drop daily until December 2022</p>
+            <p className="phase">Phase 3</p><p className="para">1000/1500 NFTs<br/>Drop daily until February 2023</p>
+            <p className="phase">Phase 4</p><p className="para">1500/1500 NFTs.Final release.<br/>Promote in April 2023</p>
                     </div>
 
                 </div>
